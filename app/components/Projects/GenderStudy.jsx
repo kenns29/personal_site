dea5321cf7db313e968343e1dc3320324caa607c
hxwang <hxwang@asu.edu>
2020-05-02 23:43:14 -0700

    updates

--- app/components/Projects/GenderStudy.jsx
+++ app/components/Projects/GenderStudy.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {bindActionCreators} from "redux";
-import {connect} from 'react-redux';
-import Project, {ProjectImg, ProjectText} from '../Project';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import Project, { ProjectImg, ProjectText } from "../Project";
 class GenderStudy extends Component {
-  render(){
+  render() {
     return (
       <Project>
         <ProjectImg>
@@ -12,10 +12,20 @@ class GenderStudy extends Component {
           </a>
         </ProjectImg>
         <ProjectText>
-          <span className = "title">A Study for Evolution of Topics in Women Study</span>
+          <span className="title">
+            A Study for Evolution of Topics in Women Study
+          </span>
           <p>
-            We have developed a visual interface to help woman study researchers explore the evolution of research topics in their field. Our data consists of 16857 research paper titles and abstracts from 10001 panels between the year 1997 and 2016. We used various methods such as topic modeling, keyword trends, and the word tree, to help the researchers explore and make sense of the dataset.
+            We have developed a visual interface to help woman study researchers
+            explore the evolution of research topics in their field. Our data
+            consists of 16857 research paper titles and abstracts from 10001
+            panels between the year 1997 and 2016. We used various methods such
+            as topic modeling, keyword trends, and the word tree, to help the
+            researchers explore and make sense of the dataset.
           </p>
+          <span>
+            <a href="https://github.com/kenns29/topic-analysis">Code</a>
+          </span>
         </ProjectText>
       </Project>
     );
@@ -27,4 +37,7 @@ function mapStateToProps(state) {
 function matchDispatchToProps(dispatch) {
   return bindActionCreators({}, dispatch);
 }
-export default connect(mapStateToProps, matchDispatchToProps)(GenderStudy);
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(GenderStudy);
